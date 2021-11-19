0fc4b665a6c9bec21815db0161a69fdbaf1bb3f9
vzvu3k6k <vzvu3k6k@gmail.com>
2021-11-20 04:03:34 +0900

    downloadFile

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
@@ -39,14 +41,35 @@ func deleteStrings(s string, dels ...string) string {
 	return s
 }
 
+func downloadFile(filename, url string) error {
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad HTTP status: %s", resp.Status)
+	}
+
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = io.Copy(f, resp.Body)
+	return err
+}
+
 func main() {
 	ctx, cancel := chromedp.NewContext(context.Background())
 	defer cancel()
 
 	twitterIds := os.Args[1:]
 	for _, id := range twitterIds {
-		src := getAvatarURL(ctx, id)
-		filename := id + filepath.Ext(src)
-		fmt.Printf("wget %s -O %s\n", src, filename)
+		url := getAvatarURL(ctx, id)
+		filename := id + filepath.Ext(url)
+		downloadFile(filename, url)
 	}
 }
